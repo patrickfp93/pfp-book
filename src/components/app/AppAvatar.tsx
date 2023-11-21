6f0361a879649a25da7c9ef9afb3bca6c9080e76
Patrick Fernandes Peçanha <patrickfp93@gmail.com>
2023-11-21 14:00:12 -0300

    refactoring2 - alfa-002

--- src/components/app/AppAvatar.tsx
+++ src/components/app/AppAvatar.tsx
@@ -1,27 +1,19 @@
-import { useFavicon, useHover, useMeasure, useWindowSize } from "@uidotdev/usehooks";
+import { useFavicon, useHover} from "@uidotdev/usehooks";
 import { Variants, motion } from "framer-motion";
 import Avatar from "rsuite/Avatar";
-import avatarImg from "./../../assets/avatar-min.png";
 import "./../../styles/components/avatar.less";
-import React from "react";
-import { Stack } from "rsuite";
+import { FlexboxGrid, Stack } from "rsuite";
 import useAspectAppLayout from "../../hooks/useAspectAppLayout";
 import AppBasicProps from "../../interfaces/AppBasicProps";
 import useThemeAppLayout from "../../hooks/useThemeAppLayout";
 
+const avatarImg = "./avatar-min.png";
+
 export function AppAvatar({ style, children }: AppBasicProps) {
     useFavicon(avatarImg);
     const [avatarHoverRef, hovering] = useHover();
-    var [avatarRef, avatarSize] = useMeasure();
-    const winSize = useWindowSize();
     const { aspectState, handleToggleAspect } = useAspectAppLayout();
     const { themeState } = useThemeAppLayout();
-
-    const avatarStyle: React.CSSProperties = {
-        ...style,
-        width: avatarSize.width ?? 0,
-        height: avatarSize.height ?? 0,
-    };
     const menuClassName = "avatar-menu-" + themeState;
     const onClick = () => {
         if (aspectState == "expand" && handleToggleAspect) handleToggleAspect()
@@ -30,46 +22,42 @@ export function AppAvatar({ style, children }: AppBasicProps) {
         if (aspectState == "collapse" && handleToggleAspect) handleToggleAspect()
     };
     const menuAnimateComand = (aspectState == "collapse") ? "show" : hovering ? "show" : "hide";
-
+    const avatarHover = { scale: 2, y: 0, x: 10, boxShadow: "0px 4px 4px 1px rgba(0, 0, 0, 0.4)" };
+    const transition = { type: "spring", stiffness: 400, damping: 15 };
     return (
-    <motion.div layout className={"container"} style={avatarStyle}
-        transition={{ type: "spring", stiffness: 200, damping: 20 }}
-        variants={variantsContainer(winSize,avatarSize)} animate={aspectState}>
-        <Stack ref={avatarHoverRef}>
-            <Avatar onClick={onClick}
-                onDoubleClick={onDoubleClick}
-                ref={avatarRef} size="lg"
-                as={motion.div} whileHover={{ scale: 2, y: 0, x: 10, boxShadow: "0px 4px 4px 1px rgba(0, 0, 0, 0.4)" }}
-                transition={{ type: "spring", stiffness: 400, damping: 15 }}
-                src={avatarImg}
-                className="avatar"
-                style={{ backgroundColor: "rgba(0,0,0,0)" }}
-                circle />
-
-            <Stack layout as={motion.div} className={menuClassName} variants={variantsMenu} animate={menuAnimateComand}>
-                {children}
-            </Stack>
-        </Stack>
-        <motion.div className="description" animate={aspectState} variants={variantsDescription}>
-            <h1>PAFEPE</h1> <h3><b>BOOK!</b></h3>
-        </motion.div>
-    </motion.div>);
+        <motion.div layout className={"container"} style={style}
+            transition={{ type: "spring", stiffness: 200, damping: 20 }}
+            variants={variantsContainer} animate={aspectState}>
+            <FlexboxGrid ref={avatarHoverRef} justify="center"> 
+                <FlexboxGrid.Item colspan={6}><Avatar onClick={onClick}
+                    draggable={false}
+                    onDoubleClick={onDoubleClick}
+                    size="lg"
+                    as={motion.div} whileHover={avatarHover}
+                    transition={transition} src={avatarImg} className="avatar"
+                    style={{ backgroundColor: "rgba(0,0,0,0)" }}
+                    circle />
+                    <Stack layout as={motion.div} className={menuClassName} variants={variantsMenu} animate={menuAnimateComand}>
+                        {children}
+                    </Stack>
+                </FlexboxGrid.Item>
+            </FlexboxGrid>
+            <motion.div className="description" animate={aspectState} variants={variantsDescription}>
+                <h1>PAFEPE</h1> <h3><b>BOOK!</b></h3>
+            </motion.div>
+        </motion.div>);
 }
 
-
-type Size = { width: number | null; height: number | null; }
-
-const variantsContainer = (winSize: Size, avatarSize : Size) => {
-    return {
-        expand: {
-            left: ((winSize.width ?? 0) / 2) - (avatarSize.width ?? 0) / 2,
-            top: ((winSize.height ?? 0) / 2) - (avatarSize.height ?? 0) / 2,
-        },
-        collapse: {
-            left: 0,
-            top: 20,
-        }
-    };
+const variantsContainer: Variants = {
+    expand: {
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+    },
+    collapse: {
+        left: 0,
+        top: 20,
+    }
 }
 const variantsDescription: Variants = {
     expand: {
@@ -82,7 +70,6 @@ const variantsDescription: Variants = {
         display: "none"
     }
 }
-
 const variantsMenu: Variants = {
     hide: {
         width: 0,
