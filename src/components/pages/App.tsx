f74a770c3cff273fdee3efb0128de16a75cbedf6
Patrick Fernandes Peçanha <patrickfp93@gmail.com>
2023-11-21 22:48:47 -0300

    update 3 delta-001

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,25 +1,24 @@
 import { Container, CustomProvider } from "rsuite";
 import { ptBR } from "rsuite/esm/locales";
-import AppFooter from "./components/layout/Footer";
-import AppHeader from "./components/layout/Header";
-import AppMenu from "./components/view/Menu";
-import AppContent from "./components/layout/Content";
-import AppReferences from "./components/view/References";
-import useThemeAppLayout from "./services/hooks/useThemeAppLayout";
+import AppFooter from "../layout/Footer";
+import AppHeader from "../layout/Header";
+import AppMenu from "../view/Menu";
+import AppContent from "../layout/Content";
+import AppReferences from "../view/References";
+import useThemeAppLayout from "../../services/hooks/useThemeAppLayout";
 
 export default function App() {
-    const {themeState} = useThemeAppLayout();
+    const { themeState } = useThemeAppLayout();
     const headerMinHeight = 100;
     const footerMinHeight = 30;
-    return (
-        <CustomProvider theme={themeState} locale={ptBR}>
+    return (<CustomProvider theme={themeState} locale={ptBR}>
             <Container>
                 <AppHeader style={{ minHeight: headerMinHeight }}>
-                    <AppMenu/>
+                    <AppMenu />
                 </AppHeader>
                 <AppContent headerMinHeight={headerMinHeight} footerMinHeight={footerMinHeight}></AppContent>
                 <AppFooter style={{ minHeight: footerMinHeight }}>
-                    <AppReferences/>
+                    <AppReferences />
                 </AppFooter>
             </Container>
         </CustomProvider>)
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './components/pages/App.tsx'
 import "./main.less";
 import { Provider } from 'react-redux';
 import { store } from './services/context/index.tsx';
